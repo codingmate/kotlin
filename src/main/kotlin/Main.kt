f67d917fc989f2c838d3045410a1a0c215a51673
codingmate <hsykys0728@gmail.com>
2022-11-06 17:18:22 +0900

    delete default example code

--- src/main/kotlin/Main.kt
+++
@@ -1,9 +0,0 @@
-class Main {
-
-}
-fun main(args: Array<String>) {
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
-}
