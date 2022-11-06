21fbb325fdca53ac8f61175e4788d2258235e0ce
codingmate <hsykys0728@gmail.com>
2022-11-06 17:09:25 +0900

    create Function

---
+++ src/main/kotlin/basic/Function.kt
@@ -0,0 +1,26 @@
+package basic
+
+class Function {
+    fun del(a:Int, b:Int) : Int {
+        return a - b
+    }
+
+    fun mul(a:Int, b:Int) : Int = a * b
+}
+
+fun main() {
+    val func = Function()
+
+    val del = func.del(7, 4)
+    val mul = func.mul(7, 4)
+
+    val result = StringBuilder()
+
+    result.append("1. del : ${del}")
+    result.append("2. mul : ${mul}")
+
+    print(result)
+    /*
+        1. del : 32. mul : 3
+     */
+}
