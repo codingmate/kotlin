dca9db1cde10e65580db0189bc5db762d2e043c6
codingmate <hsykys0728@gmail.com>
2022-11-06 17:00:48 +0900

    update ConsolePrint

--- src/main/kotlin/basic/ConsolePrint.kt
+++ src/main/kotlin/basic/ConsolePrint.kt
@@ -5,14 +5,19 @@ class ConsolePrint{
 }
 fun main(args:Array<String>) {
 
-    val printList = listOf( "print : Hello World!\n"
-                          , "println : Dreams Come true!"
-                          , "Like a printf(\"%s\")".format("Time FLies Like An Arrow"))
+    val printList = listOf( "1. print : Hello World!\n"
+                          , "2. println : Dreams Come true!"
+                          , "3. Like a printf(\"%s\")".format("Time FLies Like An Arrow"))
     print( printList[0] )
 
     println( printList[1] )
 
     print( printList[2] )
+    /*
+        print : Hello World!
+        println : Dreams Come true!
+        Like a printf("Time FLies Like An Arrow")
+     */
 }
 
 
