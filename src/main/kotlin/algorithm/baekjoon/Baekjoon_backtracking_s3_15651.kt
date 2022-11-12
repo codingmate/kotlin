adfbc99e4dd121cf7324c11897ae7e8fc51b6f0a
codingmate <hsykys0728@gmail.com>
2022-11-12 19:38:46 +0900

    solved 15651 / 323032KB, 1104ms, 996B

---
+++ src/main/kotlin/algorithm/baekjoon/Baekjoon_backtracking_s3_15651.kt
@@ -0,0 +1,43 @@
+package algorithm.baekjoon
+
+fun String.removeLastNewLine() : String {
+    if ( this.length > 1)
+        return this.substring(0, this.lastIndexOf("\n"))
+    else
+        return this
+}
+
+fun main() {
+    val NM = readln().split(" ")
+    val N = NM[0].toInt()
+    val M = NM[1].toInt()
+
+    print(Q15651(N, M).solution())
+}
+
+class Q15651( private val N:Int, private val M:Int ) {
+    fun solution () : String{
+        val result = StringBuilder()
+        val numList = ArrayList<Int>()
+
+        fun backtracking(start:Int) {
+            numList.add(start)
+
+            for ( end in 1..N ) {
+                if ( numList.size < M ) {
+                    backtracking(end)
+                }
+            } // for : end
+
+            if ( numList.size == M )
+                result.append( numList.joinToString(separator = " ", postfix = "\n") )
+
+            numList.removeLast()
+        } // fun : backtracking
+
+        for ( num in 1..N )
+            backtracking(num)
+
+        return result.toString().removeLastNewLine()
+    }
+}
