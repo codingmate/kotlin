4608acb340c69dd605aaa4ba0df862c2def310ff
codingmate <hsykys0728@gmail.com>
2022-11-13 14:13:14 +0900

    create List/Array) joinToStringBuilder

--- src/main/kotlin/algorithm/common/StringUtil.kt
+++ src/main/kotlin/algorithm/common/StringUtil.kt
@@ -1,8 +1,24 @@
 package algorithm.common
 
 fun String.removeLastNewLine() : String {
-    if ( this.length > 1)
+    if ( this.length > 1 && this.lastIndexOf("\n") > -1)
         return this.substring(0, this.lastIndexOf("\n"))
     else
         return this
+}
+
+fun <T> Iterable<T>.joinToStringBuilder(separator : CharSequence = ", ", prefix: CharSequence = "", postfix: CharSequence = ""): StringBuilder {
+    val result = StringBuilder(prefix)
+    for ( e in this ) {
+        result.append(e.toString() + separator)
+    }
+    result.append(postfix)
+    return result
+}
+fun <T> Array<out T>.joinToStringBuilder(separator : CharSequence = ", ", prefix: CharSequence = "", postfix: CharSequence = ""): StringBuilder {
+    val result = StringBuilder(prefix)
+    for ( e in this )
+        result.append(e.toString() + separator)
+    result.append(postfix)
+    return result
 }
