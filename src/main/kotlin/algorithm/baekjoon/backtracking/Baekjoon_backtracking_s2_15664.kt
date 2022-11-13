b09ed8a7b3211f35e5296932f68bae79e67521a3
codingmate <hsykys0728@gmail.com>
2022-11-13 18:51:31 +0900

    solved 15664 / 23824KB, 184ms, 1360B

---
+++ src/main/kotlin/algorithm/baekjoon/backtracking/Baekjoon_backtracking_s2_15664.kt
@@ -0,0 +1,51 @@
+package algorithm.baekjoon.backtracking
+
+import algorithm.common.removeLastNewLine
+
+fun main() {
+    val NM = readln().split(" ")
+    val N = NM[0].toInt()
+    val M = NM[1].toInt()
+
+    val numsASC = readln().split(" ").map{ it.toInt() }.sorted()
+    print(Q15664(N, M, numsASC).solution().removeLastNewLine())
+
+
+}
+
+
+class Q15664(val N:Int, val M:Int, val nums : List<Int>) {
+    fun solution() : String {
+        val result = StringBuilder()
+        val idxs = Array(M){ -1 }
+
+        fun dfs ( depth : Int ) {
+            if ( depth == M ) {
+                idxs.forEach{ result.append("${nums[it]} ") }
+                result.append("\n")
+                return
+            }
+
+            var lastNum = -1
+            for ( nextIdx in 0 until N ) {
+
+                val nextNum = nums[nextIdx]
+                when {
+                    depth > 0 && nums[idxs[depth-1]] > nextNum -> continue
+                    lastNum == nextNum -> continue
+                    idxs.contains(nextIdx) -> continue
+                }
+                idxs[depth] = nextIdx
+                lastNum = nextNum
+                dfs(depth + 1)
+                idxs[depth] = -1
+            } // for : nextIdx
+
+        }
+
+        dfs(0)
+
+
+        return result.toString()
+    }
+}
