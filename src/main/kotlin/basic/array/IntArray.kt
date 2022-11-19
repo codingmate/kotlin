4e13751f17d52b6078e2cc759fc64e2e41a8a239
codingmate <hsykys0728@gmail.com>
2022-11-19 21:46:10 +0900

    update IntArray

--- src/main/kotlin/basic/array/IntArray.kt
+++ src/main/kotlin/basic/array/IntArray.kt
@@ -2,12 +2,13 @@ package basic.array
 
 fun main () {
     println(Array().intArrayInitValues().joinToString())
-
+    // 0, 0, 0, 0, 0
     println(Array().intArrayDefaultValues().joinToString())
+    // -1, -1, -1, -1, -1
 }
 class Array {
     fun intArrayInitValues(): IntArray {
-        return IntArray(5) // 0, 0, 0, 0, 0
+        return IntArray(5)
     }
 
     fun intArrayDefaultValues(): IntArray {
