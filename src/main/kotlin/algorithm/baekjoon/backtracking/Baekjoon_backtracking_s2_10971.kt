b441d2c02093d815c7a9fed58f9c778740775e5c
codingmate <hsykys0728@gmail.com>
2022-11-19 21:20:38 +0900

    second failed!!!!! & retry...... 10971 / 01:08:47.66 :

--- src/main/kotlin/algorithm/baekjoon/backtracking/Baekjoon_backtracking_s2_10971.kt
+++ src/main/kotlin/algorithm/baekjoon/backtracking/Baekjoon_backtracking_s2_10971.kt
@@ -7,27 +7,34 @@ fun main() {
         val cols = readln().split(" ").map{ it.toInt() } as ArrayList<Int>
         matrix.add(cols)
     }
-
     print( Q10971(N, matrix).solution() )
 }
-class Q10971( val N : Int, val matrix : List<List<Int>> ) {
+class Q10971( val N : Int, private val costMatrix : List<List<Int>> ) {
     fun solution() : Int {
-        var minCost = Int.MAX_VALUE
+        val minCostMatrix = Array(N) { Array(N) { Int.MAX_VALUE } }
         val indexs = Array(N) { -1 }
         val visits = Array(N) { false }
         fun dfs( depth : Int) {
             if ( depth == N ) {
+                //println(indexs.joinToString())
                 var cost = 0
+                val startIndex = indexs[0]
+                val endIndex = indexs[N-1]
                 for ( i in 0 .. N - 2 )
-                    cost += matrix[indexs[i]][indexs[i+1]]
-                cost += matrix[indexs[N-1]][indexs[0]]
-                if ( minCost > cost )
-                    minCost = cost
+                    cost += costMatrix[indexs[i]][indexs[i+1]]
+                //println( "${minCostMatrix[startIndex][endIndex]} ::: ${cost}" )
+                if ( minCostMatrix[startIndex][endIndex] > cost )
+                    minCostMatrix[startIndex][endIndex] = cost
+                //println( "${minCostMatrix[startIndex][endIndex]} ::: ${cost}" )
+
                 return
             }
 
             for ( nextIndex in 0 until N ) {
-                if ( visits[nextIndex])
+                if ( visits[nextIndex] )
+                    continue
+                if ( depth > 0
+                    && costMatrix[indexs[depth-1]][nextIndex] == 0)
                     continue
 
                 visits[nextIndex] = true
@@ -38,6 +45,23 @@ class Q10971( val N : Int, val matrix : List<List<Int>> ) {
             }
         }
         dfs(0)
+        var minCost = Int.MAX_VALUE
+//        for ( cols in minCostMatrix ) {
+//            println( cols.joinToString() )
+//        }
+
+        for ( row in 0 until N-1)
+            for ( col in 0 until N-1)
+                if ( row == col )
+                    continue
+                else {
+                    val cost = minCostMatrix[row][col]
+                    if ( minCost > cost )
+                        minCost = cost
+                }
+        for ( cols in minCostMatrix ) {
+            println(cols.joinToString())
+        }
         return minCost
     }
 }
