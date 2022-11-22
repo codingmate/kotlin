97f03f3f0f97aec8097050ea093e9617b88566f8
codingmate <hsykys0728@gmail.com>
2022-11-22 21:22:47 +0900

    solved 10971 / 21284KB, 444ms, 1342B / using 15 min

--- src/main/kotlin/algorithm/baekjoon/backtracking/Baekjoon_backtracking_s2_10971.kt
+++ src/main/kotlin/algorithm/baekjoon/backtracking/Baekjoon_backtracking_s2_10971.kt
@@ -3,82 +3,42 @@ package algorithm.baekjoon.backtracking
 fun main() {
     val N = readln().toInt()
     val matrix = Array(N){ IntArray(N) }
-    for ( row in 0 until N ) {
-        val cols = readln().split(" ").map{ it.toInt() }.toIntArray()
-        matrix[row] = cols
-    }
+    for ( row in 0 until N )
+        matrix[row] = readln().split(" ").map{ it.toInt() }.toIntArray()
     print( Q10971(N, matrix).solution() )
 }
 class Q10971( val N : Int, private val costMatrix : Array<IntArray> ) {
     fun solution() : Int {
-        val goMinCostMatrix = Array(N) { Array(N) { Int.MAX_VALUE } }
-        val backMinCostMatrix = Array(N){ Array(N) { Int.MAX_VALUE } }
-        for ( row in 0 until N )
-            for ( col in 0 until N )
-                backMinCostMatrix[row][col] = costMatrix[row][col]
 
-
-        val visits = BooleanArray(N) { false }
-        val indexs = IntArray(N) { -1 }
-
-        var backMinCost = 0
+        var minResult = Int.MAX_VALUE
+        val indexs = Array(N) { -1 }
+        val visits = Array(N) { false }
         fun dfs( depth: Int ) {
             if ( depth == N ) {
-                var cost = 0
-                for ( i in 0 .. N-2 )
-                    cost += costMatrix[indexs[i]][indexs[i+1]]
-                goMinCostMatrix[indexs[0]][indexs[N-1]] = minOf(goMinCostMatrix[indexs[0]][indexs[N-1]], cost)
+                var result = 0
+                if ( costMatrix[indexs[N-1]][indexs[0]] == 0 )
+                    return
+                for ( i in 0 until N - 1 )
+                    result += costMatrix[indexs[i]][indexs[i+1]]
+                result += costMatrix[indexs[N-1]][indexs[0]]
+                minResult = minOf(minResult, result)
                 return
             }
-            for ( nextIndex in 0 until N ) {
-                if ( visits[nextIndex] )
+            for ( next in 0 until N ) {
+                if ( visits[next] )
                     continue
-                if ( depth > 0 ) {
-                    when {
-                        costMatrix[indexs[depth - 1 ]][nextIndex] == 0 -> continue
-                    }
-                }
-                visits[nextIndex] = true
-                indexs[depth] = nextIndex
+                if ( depth > 0
+                    && costMatrix[indexs[depth-1]][next] == 0)
+                    continue
 
-
-                val nextMoveCost = if ( depth > 0 ) costMatrix[indexs[depth-1]][indexs[depth]] else 0
-                backMinCost += nextMoveCost
-                if ( depth > 0 ) {
-                    val prevMin = backMinCostMatrix[indexs[0]][indexs[depth]]
-                    if ( prevMin > backMinCost ) {
-                        backMinCostMatrix[indexs[0]][indexs[depth]] = backMinCost
-                    }
-                }
+                visits[next] = true
+                indexs[depth] = next
                 dfs(depth + 1)
-
-                visits[nextIndex] = false
-                indexs[depth] = -1
-                backMinCost -= nextMoveCost
-            }
-        } // dfs
-        dfs(0)
-
-//        println("go")
-//        for ( cols in goMinCostMatrix )
-//            println(cols.joinToString())
-//        println("back")
-//        for ( cols in backMinCostMatrix )
-//            println(cols.joinToString())
-
-        var minCost = Int.MAX_VALUE
-        for ( row in 0 until N ) {
-            for ( col in 0 until N ) {
-                if ( row == col )
-                    continue
-                else {
-                    val cost = goMinCostMatrix[row][col] + backMinCostMatrix[col][row]
-                    //println( "${goMinCostMatrix[row][col]} :: ${backMinCostMatrix[col][row]} = ${cost}" )
-                    if ( minCost > cost )
-                        minCost = cost
-                }
+                visits[next] = false
             }
         }
-        return minCost
+        dfs(0)
+
+        return minResult
     } // solution
 }
