a2dc3c7e4a5f5769d865498d4d12733815a31d0d
codingmate <hsykys0728@gmail.com>
2022-12-01 20:26:42 +0900

    solved 2606 / 14556KB, 120ms, 722B / 3L -> 1W / using 1 hour

---
+++ src/main/kotlin/algorithm/baekjoon/bfs/Baekjoon_bfs_s3_2606.kt
@@ -0,0 +1,46 @@
+package algorithm.baekjoon.bfs
+
+import java.util.LinkedList
+
+val N = readln().toInt()
+val M = readln().toInt()
+val matrix = Array(N + 1) { IntArray(N + 1) }
+val visits = BooleanArray(N + 1)
+
+fun main() {
+	for ( row in 0 until M ) {
+		val rc = readln().split(" ")
+		val r = rc[0].toInt()
+		val c = rc[1].toInt()
+		matrix[r][c] = 1
+		matrix[c][r] = 1
+	}
+	print( Q2606().solution() )
+
+}
+
+class Q2606 {
+	fun solution(): Int {
+		val q = LinkedList<Int>()
+		q.add(1)
+
+		while( q.size > 0 ) {
+			val from = q[0]
+			q.removeFirst()
+			visits[from] = true
+
+			for ( to in 2 .. N )
+				if ( matrix[from][to] == 0 || visits[to] )
+					continue
+				else
+					q.add(to)
+		}
+		var count = 0
+		for ( num in 2..N )
+			if ( visits[num] )
+				count++
+
+		return count
+	}
+
+}
