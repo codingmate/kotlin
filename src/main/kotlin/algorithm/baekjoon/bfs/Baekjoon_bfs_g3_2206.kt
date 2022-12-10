191d67fd07e3fbe12bbaa7a6467e8342cc1c243e
codingmate <hsykys0728@gmail.com>
2022-12-10 16:20:01 +0900

    failed 2206................ // using 10 hour? fu**ing

--- src/main/kotlin/algorithm/baekjoon/bfs/Baekjoon_bfs_g3_2206.kt
+++ src/main/kotlin/algorithm/baekjoon/bfs/Baekjoon_bfs_g3_2206.kt
@@ -19,85 +19,48 @@ fun main() {
 class Q2206 {
 	fun solution( R: Int, C: Int, matrix:Array<IntArray> ): Int {
 		data class Dot(val r: Int, val c: Int)
-
+		if ( R == 1 && C == 1 )
+			return 1
 		val dr = arrayOf(0, 0, 1, -1)
 		val dc = arrayOf(1, -1, 0, 0)
 
-		fun noBreakBfs(): Int{
-			val visits = Array(R) { BooleanArray(C) }
-			val costMatrix = Array(R) { IntArray(C) }
-			val q = LinkedList<Dot>()
-			q.add(Dot(0, 0))
-			visits[0][0] = true
-			costMatrix[0][0] = 1
-			costMatrix[R-1][C-1] = -1
+		val costs = Array(R) { IntArray(C) }
+		val walls = Array(R) { IntArray(C) }
 
-			while ( q.size > 0 ) {
-				val from = q[0]
-				q.removeFirst()
+		val q = LinkedList<Dot>()
+		q.add(Dot(0, 0))
+		costs[0][0] = 1
 
-				for ( i in dr.indices ) {
-					val nR = from.r + dr[i]
-					val nC = from.c + dc[i]
-					if ( nR < 0 || nC < 0 || nR >= R || nC >= C || visits[nR][nC] || matrix[nR][nC] == 1)
-						continue
-					val to = Dot(nR, nC)
-					q.add(to)
-					costMatrix[to.r][to.c] = costMatrix[from.r][from.c] + 1
-					visits[to.r][to.c] = true
-				}
-			}
-			return costMatrix[R-1][C-1]
- 		}
-		fun bfs(): Int{
-			val costMatrix = Array(R) { IntArray(C) }
-			val breakMatrix = Array(R) { IntArray(C) }
-			costMatrix[0][0] = 1
-			costMatrix[R-1][C-1] = -1
-			val q = LinkedList<Dot>()
+		while( q.size > 0 ) {
+			val from = q[0]
+			q.removeFirst()
 
-			q.add(Dot(0, 0))
-			while ( q.size > 0 ) {
-				val from = q[0]
-				q.removeFirst()
-				val fromCost = costMatrix[from.r][from.c]
-				val fromBreak = breakMatrix[from.r][from.c]
-				for ( i in dr.indices ) {
-					val nr = from.r + dr[i]
-					val nc = from.c + dc[i]
-					if ( nr < 0 || nc < 0 || nr >= R || nc >= C )
-						continue
 
-					if ( fromBreak + matrix[nr][nc] > 1 )
-						continue
+			for ( i in dr.indices ) {
+				val toR = from.r + dr[i]
+				val toC = from.c + dc[i]
 
-					val to = Dot(nr, nc)
-					if ( fromBreak == breakMatrix[to.r][to.c] && costMatrix[to.r][to.c] > 0 && fromCost + 1 > costMatrix[to.r][to.c] )
-						continue
+				if ( toR == R - 1 && toC == C - 1 )
+					return costs[from.r][from.c] + 1
 
-					q.add(to)
-					val toBreak = fromBreak + matrix[nr][nc]
-					costMatrix[to.r][to.c] = fromCost + 1
-					breakMatrix[to.r][to.c] = toBreak
-				}
+				if ( toR < 0 || toC < 0 || toR >= R || toC >= C
+						|| walls[from.r][from.c] + matrix[toR][toC] > 1 )
+					continue
+
+				if ( toR == R - 1 && toC == C - 1 )
+					return costs[from.r][from.c] + 1
+
+				costs[toR][toC] = costs[from.r][from.c] + 1
+
+				walls[toR][toC] = walls[from.r][from.c] + matrix[toR][toC]
 				for ( r in 0 until R )
-					println("${costMatrix[r].joinToString()} || ${breakMatrix[r].joinToString()}")
-				println()
-			} // while : q
+					println("${costs[r].joinToString()} ::: ${walls[r].joinToString()} ")
+				q.add(Dot(toR, toC))
 
-			return costMatrix[R-1][C-1]
-		} // fun : bfs
-
-		var minDist = noBreakBfs()
-		val dist = bfs()
-
-		when {
-			minDist > 0 && dist > 0 -> minDist = minOf(minDist, dist)
-			minDist == -1 -> minDist = dist
+			}
 		}
 
 
-
-		return minDist
+		return -1
 	}
 }
