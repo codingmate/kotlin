46bdd0e9ea184b72cebc83de2508e3ecffa018f6
codingmate <hsykys0728@gmail.com>
2022-12-15 22:02:56 +0900

    solved 16236 / 27744KB, 312ms, 1886B / spend 7 hour!!!!!!!
    3failed, 3day

--- src/main/kotlin/algorithm/baekjoon/bfs/Baekjoon_bfs_g3_16236.kt
+++ src/main/kotlin/algorithm/baekjoon/bfs/Baekjoon_bfs_g3_16236.kt
@@ -1,7 +1,6 @@
 package algorithm.baekjoon.bfs
 
 import java.util.LinkedList
-import kotlin.math.abs
 
 fun main() {
 	val N = readln().toInt()
@@ -17,74 +16,76 @@ fun main() {
 
 class Q16236 {
 	fun solution( N: Int, matrix: Array<IntArray> ): Int {
-		data class Dot(var r: Int, var c: Int, var size: Int)
+		data class Dot(var r: Int, var c: Int)
 
 		val R = N
 		val C = N
-		var shark = Dot(0, 0, 0)
-		val fishs = LinkedList<Dot>()
+		val shark = Dot(-1,-1)
+
 		for ( r in 0 until R )
 			for ( c in 0 until C )
-				when ( matrix[r][c] ) {
-					0    -> {}
-					9    -> {
-						shark = Dot(r, c, 2)
-						matrix[r][c] = 0
-					}
-					else -> fishs.add(Dot(r, c, matrix[r][c]))
+				if ( matrix[r][c] == 9 ) {
+					shark.r = r
+					shark.c = c
+					matrix[r][c] = 0
 				}
 
-
 		var totalDistance = 0
+		var sharkSize = 2
 		var eatCount = 0
-		val dr = arrayOf(-1, 0, 0, 1) // 위, 왼, 우, 밑 순서
+
+		val dr = arrayOf(-1, 0, 0, 1) // 상, 좌, 우, 하
 		val dc = arrayOf(0, -1, 1, 0)
-		while (fishs.any { it.size < shark.size }) { // 먹을 수 있는 물고기가 남아있는가
+
+		var ate = true
+
+		while ( ate ) {
+			ate = false
+
 			val q = LinkedList<Dot>()
-			val costs = Array(R) { IntArray(C) { -1 } }
+			val costs = Array(R){ IntArray(C) { -1 } }
+
 			q.add(shark)
 			costs[shark.r][shark.c] = 0
-
-			var foundFish = false
-			while ( q.size > 0 && !foundFish ) {
-
+			while ( q.size > 0 && !ate ) {
 				val from = q[0]
 				q.removeFirst()
+
+				if ( matrix[from.r][from.c] in 1 until sharkSize ) {
+					eatCount++
+					if ( eatCount == sharkSize ) {
+						eatCount = 0
+						sharkSize++
+					}
+					totalDistance += costs[from.r][from.c]
+					shark.r = from.r
+					shark.c = from.c
+					matrix[shark.r][shark.c] = 0
+					ate = true
+					break
+				}
 				for ( i in dr.indices ) {
 					val toR = from.r + dr[i]
 					val toC = from.c + dc[i]
-					if ( toR < 0 || toC < 0 || toR >= R || toC >= C || costs[toR][toC] > -1 || matrix[toR][toC] > shark.size )
+
+					if ( toR < 0 || toC < 0 || toR >= R || toC >= C || costs[toR][toC] > -1 || matrix[toR][toC] > sharkSize )
 						continue
-					costs[toR][toC] = costs[from.r][from.c] + 1
-					
-					if ( matrix[toR][toC] > 0
-							&& shark.size > matrix[toR][toC] ) { // 다음 칸에 먹을 수 있는 물고기가 있을 경우
-						for ( i in fishs.indices ) { // 먹을 물고기 제거
-							val fish = fishs[i]
-							if ( fish.r == toR && fish.c == toC ) {
-								fishs.remove(fish)
-								break
-							}
-						}
-						matrix[toR][toC] = 0
-						totalDistance += costs[toR][toC]
+					val toCost = costs[from.r][from.c] + 1
 
-						if ( ++eatCount == shark.size ) {
-							eatCount = 0
-							shark.size++
-						}
-						shark.r = toR
-						shark.c = toC
-						foundFish = true
-						break
-					} else if ( matrix[toR][toC] == 0 )
-						q.add(Dot(toR, toC, 0))
+					q.add(Dot(toR, toC))
+					costs[toR][toC] = toCost
+				} // for
+				q.sortWith { a1, a2 ->
+					if ( costs[a1.r][a1.c] == costs[a2.r][a2.c] )
+						if ( a1.r == a2.r )
+							a1.c - a2.c
+						else
+							a1.r - a2.r
+					else
+						costs[a1.r][a1.c] - costs[a2.r][a2.c]
 				}
-			}
-		} // while
-
-
-
+			} // while
+		}
 		return totalDistance
 	}
 }
